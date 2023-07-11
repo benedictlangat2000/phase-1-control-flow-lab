431c81e9e2a1eaf6450acb46af173fbf55541786
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-07-11 11:10:49 +0300

    git@github.com:benedictlangat2000/phase-1-control-flow-lab.git

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+
 }
 
 function ternaryCheckCity(){
@@ -8,4 +9,32 @@ function ternaryCheckCity(){
 
 function switchOnCharmFromTip(){
   // Write your code here!
+}
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  } else {
+    // For distances between 400 and 2000 feet (exclusive), the cost is not mentioned in the original code.
+    // You can add additional logic here if needed.
+    return 'This ride has a variable cost. Please contact the driver for pricing.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
